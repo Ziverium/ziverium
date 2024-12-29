9f74d51a7e1a9cf3998d0a16ee0030a118931bf1
Alias_Alex <shaikh.injamul06905@gmail.com>
2024-12-30 00:14:36 +0530

    port updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5000, // Replace with the desired port number
+    host: "0.0.0.0", // Ensure the server is accessible externally
+  },
   css: {
     preprocessorOptions: {
       sass: {
